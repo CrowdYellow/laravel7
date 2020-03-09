856763e80def62a1334f981a39a7e097a2c63f03
CrowdYellow <echo61546@gmail.com>
2020-03-09 16:17:27 +0800

    使用JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -42,5 +43,15 @@ class User extends Authenticatable
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,26 @@ Route::prefix('v1/frontend')
             ->group(function () {
                 // 图片验证码
                 Route::post('captchas', 'CaptchasController@store')
-                    ->name('captchas.store');
+                    ->name('api.captchas.store');
                 // 短信验证码
                 Route::post('verificationCodes', 'VerificationCodesController@store')
-                    ->name('verificationCodes.store');
+                    ->name('api.verificationCodes.store');
                 // 用户注册
                 Route::post('users', 'UsersController@store')
-                    ->name('users.store');
+                    ->name('api.users.store');
                 // 第三方登录
                 Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
                     ->where('social_type', 'weixin')
-                    ->name('socials.authorizations.store');
+                    ->name('api.socials.authorizations.store');
+                // 登录
+                Route::post('authorizations', 'AuthorizationsController@store')
+                    ->name('api.authorizations.store');
+                // 刷新token
+                Route::put('authorizations/current', 'AuthorizationsController@update')
+                    ->name('authorizations.update');
+                // 删除token
+                Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+                    ->name('authorizations.destroy');
             });
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
