91def071d113c9253b5b5a66dd878c6101312f1f
CrowdYellow <echo61546@gmail.com>
2020-03-09 13:01:32 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 
 Route::prefix('v1/frontend')->name('api.v1.')->namespace('Api\Frontend')->group(function() {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
     // 用户注册
     Route::post('users', 'UsersController@store')
         ->name('users.store');
