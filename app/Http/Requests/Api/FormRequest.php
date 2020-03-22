699227b42e86e73951688fd752a40f03a9235f9c
CrowdYellow <echo61546@gmail.com>
2020-03-22 17:49:46 +0800

    添加辅助文件

--- app/Http/Requests/Api/FormRequest.php
+++ app/Http/Requests/Api/FormRequest.php
@@ -10,4 +10,14 @@ class FormRequest extends BaseFormRequest
     {
         return true;
     }
+
+    /**
+     * 获取路由对应的控制器中的方法
+     * @return false|string
+     */
+    public function getFunction()
+    {
+        $str = $this->route()->getAction()['uses'];
+        return substr($str,strpos($str,"@") + 1);
+    }
 }
