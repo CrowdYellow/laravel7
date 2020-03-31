33717c4e64f92fc3d4e6c16bbe96aa8852f24738
CrowdYellow <echo61546@gmail.com>
2020-03-31 23:10:45 +0800

    去除xss过滤中自动添加p标签

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use function GuzzleHttp\Psr7\uri_for;
 
 class TopicResource extends JsonResource
 {
@@ -14,9 +15,17 @@ class TopicResource extends JsonResource
      */
     public function toArray($request)
     {
+        $btn  = false;
+        $user = $request->user();
+        if ($user) {
+            if ($user->id == $request->user_id) {
+                $btn = true;
+            }
+        }
         $data             = parent::toArray($request);
         $data['user']     = new UserResource($this->whenLoaded('user'));
         $data['category'] = new CategoryResource($this->whenLoaded('category'));
+        $data['btn']      = (int) $btn;
 
         return $data;
     }
